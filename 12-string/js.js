fed1745ba5f7101e48b1d5944949da92f608e003
Freivin Campbell <freivin12@gmail.com>
2020-03-09 16:59:56 -0600

    12 strings with js

---
+++ 12-string/js.js
@@ -0,0 +1,42 @@
+let x = "John Doe";
+
+let carName1 = "Volvo XC60";  // Double quotes
+let carName2 = 'Volvo XC60';  // Single quotes
+
+let answer1 = "It's alright";
+let answer2 = "He is called 'Johnny'";
+let answer3 = 'He is called "Johnny"';
+
+let txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let sln = txt.length;
+console.log(sln);
+
+
+let a = "We are the so-called \"Vikings\" from the north.";
+let b = 'It\'s alright.';
+let c = "The character \\ is called backslash.";
+
+
+document.getElementById("demo").innerHTML =
+    "Hello Dolly!";
+
+document.getElementById("demo").innerHTML = "Hello \
+Dolly!";
+
+document.getElementById("demo").innerHTML = "Hello " +
+    "Dolly!";
+
+
+// Strings Can be Objects
+let d = "John";
+let e = String("John");
+
+// typeof x will return string
+// typeof y will return object
+
+
+
+
+
+
+
