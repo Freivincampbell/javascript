d7e6b5dfe953b875e49173b1ccd3dfd4f7b0e2a7
Freivin Campbell <freivin12@gmail.com>
2020-03-10 08:13:00 -0600

    13 String - concat method

---
+++ 13-string_methods/concat.js
@@ -0,0 +1,13 @@
+// concat() joins two or more strings:
+
+const text1 = "Hello";
+const text2 = "World";
+const text3 = text1.concat(" ", text2);
+
+console.log(text3);
+
+let text4 = "Hello" + " " + "World!";
+let text5 = "Hello".concat(" ", "World!");
+
+console.log(text4, text5);
+
