fe581b8ad59ed193e2beeed25fd3113b5bc22285
Freivin Campbell <freivin12@gmail.com>
2020-03-10 08:13:08 -0600

    13 String - replace method

---
+++ 13-string_methods/replace.js
@@ -0,0 +1,15 @@
+// Replacing String Content
+// The replace() method replaces a specified value with another value in a string:
+
+let str = "Please visit Microsoft!";
+let n = str.replace("Microsoft", "W3Schools");
+
+console.log(n);
+
+// By default, the replace() method replaces only the first match:
+
+let str2 = "Please visit Microsoft and Microsoft!";
+n = str.replace("Microsoft", "W3Schools");
+
+console.log(n);
+
