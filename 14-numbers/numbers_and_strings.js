663dc0467cfce8d34d1048cbe5d6d6eb4c0cb51e
Freivin Campbell <freivin12@gmail.com>
2020-03-10 08:31:56 -0600

    14 numbers - numbers and strings

---
+++ 14-numbers/numbers_and_strings.js
@@ -0,0 +1,46 @@
+// Adding Numbers and Strings
+
+var x = 10;
+var y = 20;
+var z = x + y;           // z will be 30 (a number)
+
+var x = "10";
+var y = "20";
+var z = x + y;           // z will be 1020 (a string)
+
+var x = 10;
+var y = "20";
+var z = x + y;           // z will be 1020 (a string)
+
+var x = "10";
+var y = 20;
+var z = x + y;           // z will be 1020 (a string)
+
+var x = 10;
+var y = 20;
+var z = "The result is: " + x + y;
+
+var x = 10;
+var y = 20;
+var z = "30";
+var result = x + y + z;
+
+var x = 100;         // x is a number
+var y = "100";       // y is a string
+
+var x = "100";
+var y = "10";
+var z = x / y;       // z will be 10
+
+var x = "100";
+var y = "10";
+var z = x * y;       // z will be 1000
+
+var x = "100";
+var y = "10";
+var z = x - y;       // z will be 90
+
+var x = "100";
+var y = "10";
+var z = x + y;       // z will not be 110 (It will be 10010)
+
