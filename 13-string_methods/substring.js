58f2160d320536eed8bbb8a391251b1dc6dc0129
Freivin Campbell <freivin12@gmail.com>
2020-03-09 20:01:44 -0600

    13 String - substring method

---
+++ 13-string_methods/substring.js
@@ -0,0 +1,7 @@
+// substring() is similar to slice().
+// The difference is that substring() cannot accept negative indexes.
+
+let str = "Apple, Banana, Kiwi";
+let res = str.substring(7, 13);
+
+console.log(res);
