e3d14f3d63ade786739bbf914435f28c830afcb0
Freivin Campbell <freivin12@gmail.com>
2020-03-10 08:31:45 -0600

    14 numbers - numbers

---
+++ 14-numbers/numbers.js
@@ -0,0 +1,24 @@
+// JavaScript has only one type of number. Numbers can be written with or without decimals.
+
+
+let x = 3.14;    // A number with decimals
+let y = 3;       // A number without decimals
+console.log(x);
+console.log(y);
+
+ x = 123e5;    // 12300000
+ y = 123e-5;   // 0.00123
+console.log(x);
+console.log(y);
+
+
+ x = 999999999999999;   // x will be 999999999999999
+ y = 9999999999999999;  // y will be 10000000000000000
+console.log(x);
+console.log(y);
+
+ x = 0.2 + 0.1;         // x will be 0.30000000000000004
+console.log(x);
+
+ x = (0.2 * 10 + 0.1 * 10) / 10;       // x will be 0.3
+console.log(x);
