663a6aa5f57019ec7c734082c5837931b1ec561f
Freivin Campbell <freivin12@gmail.com>
2020-03-10 22:20:41 -0600

    15 number - parseInt method

---
+++ 15-numbers_methods/parseInt.js
@@ -0,0 +1,7 @@
+// parseInt() parses a string and returns a whole number. Spaces are allowed. Only the first number is returned:
+
+parseInt("10");         // returns 10
+parseInt("10.33");      // returns 10
+parseInt("10 20 30");   // returns 10
+parseInt("10 years");   // returns 10
+parseInt("years 10");   // returns NaN
