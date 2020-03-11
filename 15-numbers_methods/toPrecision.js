5659dff5ccc533372213ba89865adbc4a923bf97
Freivin Campbell <freivin12@gmail.com>
2020-03-10 22:21:33 -0600

    15 number - toPrecision  method

---
+++ 15-numbers_methods/toPrecision.js
@@ -0,0 +1,7 @@
+// toPrecision() returns a string, with a number written with a specified length:
+
+let x = 9.656;
+x.toPrecision();        // returns 9.656
+x.toPrecision(2);       // returns 9.7
+x.toPrecision(4);       // returns 9.656
+x.toPrecision(6);       // returns 9.65600
