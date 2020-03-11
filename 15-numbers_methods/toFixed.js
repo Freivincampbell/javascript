0dbfa1eea501022079b0b98bd07ecfecfd354565
Freivin Campbell <freivin12@gmail.com>
2020-03-10 22:21:21 -0600

    15 number -  toFixed method

---
+++ 15-numbers_methods/toFixed.js
@@ -0,0 +1,7 @@
+// toFixed() returns a string, with the number written with a specified number of decimals:
+
+let x = 9.656;
+x.toFixed(0);           // returns 10
+x.toFixed(2);           // returns 9.66
+x.toFixed(4);           // returns 9.6560
+x.toFixed(6);           // returns 9.656000
