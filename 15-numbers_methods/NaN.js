caaa3429288ec50686acd3eab09a3ac651d3ac00
Freivin Campbell <freivin12@gmail.com>
2020-03-10 22:19:56 -0600

    15 number -  NaN method

---
+++ 15-numbers_methods/NaN.js
@@ -0,0 +1,7 @@
+// NaN is a JavaScript reserved word indicating that a number is not a legal number.
+// Trying to do arithmetic with a non-numeric string will result in NaN (Not a Number):
+
+let x = Number.NaN;
+
+let y = 100 / "Apple";  // x will be NaN (Not a Number)
+console.log(y); 
