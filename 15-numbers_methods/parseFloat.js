44aeb3c02742334fce4fcceb4e26e2286a933473
Freivin Campbell <freivin12@gmail.com>
2020-03-10 22:20:29 -0600

    15 number -  parse float method

---
+++ 15-numbers_methods/parseFloat.js
@@ -0,0 +1,7 @@
+// parseFloat() parses a string and returns a number. Spaces are allowed. Only the first number is returned:
+
+parseFloat("10");        // returns 10
+parseFloat("10.33");     // returns 10.33
+parseFloat("10 20 30");  // returns 10
+parseFloat("10 years");  // returns 10
+parseFloat("years 10");  // returns NaN
