a8f614d3c13ddd6014df9dd9e83819c460361527
Freivin Campbell <freivin12@gmail.com>
2020-03-10 22:21:46 -0600

    15 number - toString  method

---
+++ 15-numbers_methods/toString.js
@@ -0,0 +1,7 @@
+// The toString() method returns a number as a string.
+// All number methods can be used on any type of numbers (literals, variables, or expressions):
+
+let x = 123;
+x.toString();            // returns 123 from variable x
+(123).toString();        // returns 123 from literal 123
+(100 + 23).toString();   // returns 123 from expression 100 + 23
