1a1096d875e9e19bb5c95ba3f11b0e15eb5f053a
Freivin Campbell <freivin12@gmail.com>
2020-03-13 12:17:05 -0600

    17 array - push method

---
+++ 17-array_methods/push.js
@@ -0,0 +1,12 @@
+// Pushing
+// The push() method adds a new element to an array (at the end):
+
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.push("Kiwi");       //  Adds a new element ("Kiwi") to fruits
+console.log(fruits);
+
+// The push() method returns the new array length:
+fruits = ["Banana", "Orange", "Apple", "Mango"];
+let x = fruits.push("Kiwi");   //  the value of x is 5
+console.log(x);
+
