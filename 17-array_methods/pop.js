64d13a7d6b88ce56dfbf441b404e59986a0ed9b3
Freivin Campbell <freivin12@gmail.com>
2020-03-13 12:16:57 -0600

    17 array - pop methods

---
+++ 17-array_methods/pop.js
@@ -0,0 +1,11 @@
+// The pop() method removes the last element from an array:
+
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.pop();              // Removes the last element ("Mango") from fruits
+console.log(fruits);
+
+// The pop() method returns the value that was "popped out":
+
+fruits = ["Banana", "Orange", "Apple", "Mango"];
+let x = fruits.pop();      // the value of x is "Mango"
+console.log(x);
