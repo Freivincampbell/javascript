4a270f517adff6725a2e5123065f92bb1c79fede
Freivin Campbell <freivin12@gmail.com>
2020-03-13 12:16:50 -0600

    17 array - join method

---
+++ 17-array_methods/join.js
@@ -0,0 +1,5 @@
+// The join() method also joins all array elements into a string.
+// It behaves just like toString(), but in addition you can specify the separator:
+
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.join(" * "));
