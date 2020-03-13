f1c7fbe1dc1567068fb31ea36aec9c9aebf0059b
Freivin Campbell <freivin12@gmail.com>
2020-03-13 12:17:49 -0600

    17 array - toString method

---
+++ 17-array_methods/toString.js
@@ -0,0 +1,15 @@
+// Converting Arrays to Strings
+// The JavaScript method toString() converts an array to a string of (comma separated) array values.
+
+
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.toString());
+
+
+// JavaScript automatically converts an array to a comma separated string when a primitive value is expected.
+// This is always the case when you try to output an array.
+// These two examples will produce the same result:
+
+// All JavaScript objects have a toString() method.
+
+
