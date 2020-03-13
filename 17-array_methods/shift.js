c2d25916299e0be10e05d4f2fe26aef75dbdfa02
Freivin Campbell <freivin12@gmail.com>
2020-03-13 12:17:15 -0600

    17 array - shift method

---
+++ 17-array_methods/shift.js
@@ -0,0 +1,5 @@
+// The shift() method returns the string that was "shifted out":
+
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+let x = fruits.shift();    // the value of x is "Banana"
+console.log(fruits, x);
