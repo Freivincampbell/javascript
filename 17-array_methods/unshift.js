d5c2e2de85775f34e2c4ae91ec814ada5d06b938
Freivin Campbell <freivin12@gmail.com>
2020-03-13 12:17:58 -0600

    17 array - unshift method

---
+++ 17-array_methods/unshift.js
@@ -0,0 +1,11 @@
+// The unshift() method adds a new element to an array (at the beginning), and "unshifts" older elements
+
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.unshift("Lemon");    // Adds a new element "Lemon" to fruits
+console.log(fruits);
+
+fruits = ["Banana", "Orange", "Apple", "Mango"];
+let x = fruits.unshift("Lemon");    // Adds a new element "Lemon" to fruits
+console.log(x);
+
+
