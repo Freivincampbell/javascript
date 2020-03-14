f3e3881c26011ab87ea06e1c3df9e5e1c536c5a3
Freivin Campbell <freivin12@gmail.com>
2020-03-13 22:07:46 -0600

    18 array sorting - reverse method

---
+++ 18-array-sorting/reverse.js
@@ -0,0 +1,7 @@
+// The reverse() method reverses the elements in an array.
+// You can use it to sort an array in descending order:
+
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.reverse();     // Then reverse the order of the elements
+
+console.log(fruits);
