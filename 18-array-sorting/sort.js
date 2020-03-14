98ba70e4e4fa9451d4839b88199130711bf53921
Freivin Campbell <freivin12@gmail.com>
2020-03-13 22:07:32 -0600

    18 array sorting - sort method

---
+++ 18-array-sorting/sort.js
@@ -0,0 +1,6 @@
+// The sort() method sorts an array alphabetically:
+
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.sort();        // Sorts the elements of fruits
+
+console.log(fruits);
