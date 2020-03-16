98d0b8528c9b58d3a7117b8d6a91333d85beff95
Freivin Campbell <freivin12@gmail.com>
2020-03-15 22:40:32 -0600

    19 array iteration - indexOf method

---
+++ 19-array-iteration/indexOf.js
@@ -0,0 +1,9 @@
+// The indexOf() method searches an array for an element value and returns its position.
+// indexOf() method returns the first element found.
+
+const fruits = ["Apple", "Orange", "Apple", "Mango"];
+let a = fruits.indexOf("Apple");
+
+console.log(a);
+
+//returns -1 if the item is not found.
