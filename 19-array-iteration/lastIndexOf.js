f0fffd2ad32022117f396186ae6ddaac5eb28fe9
Freivin Campbell <freivin12@gmail.com>
2020-03-15 22:40:45 -0600

    19 array iteration - lastIndexOf method

---
+++ 19-array-iteration/lastIndexOf.js
@@ -0,0 +1,5 @@
+// Array.lastIndexOf() is the same as Array.indexOf(), but returns the position of the last occurrence of the specified element.
+
+const fruits = ["Apple", "Orange", "Apple", "Mango"];
+let a = fruits.lastIndexOf("Apple");
+console.log(a);
